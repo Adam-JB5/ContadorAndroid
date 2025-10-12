f718f71f7759c90bb69c5d5ab4a40d6c207e3be4
adam.janah <adam.janah@geoaitech.com>
2025-10-12 20:28:55 +0200

    Añadida funcion boton de resta y cambio de color

--- app/src/main/java/com/example/contador/MainActivity.kt
+++ app/src/main/java/com/example/contador/MainActivity.kt
@@ -7,7 +7,24 @@ import com.example.contador.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var contador = 0
+    private var contador = 0;
+
+    override fun onSaveInstanceState(outState: Bundle) {
+
+        super.onSaveInstanceState(outState)
+
+        outState.putInt("contador", contador)
+
+    }
+
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+
+        super.onRestoreInstanceState(savedInstanceState)
+
+        contador = savedInstanceState.getInt("contador")
+
+        binding.tvCounter.text = contador.toString() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +36,11 @@ class MainActivity : AppCompatActivity() {
         // Ahora accede a las vistas directamente
         binding.btnIncrement.setOnClickListener {
             contador++
+            binding.tvCounter.text = contador.toString()
+        }
+
+        binding.btnDecrease.setOnClickListener {
+            contador--
             binding.tvCounter.text = contador.toString()
         }
     }
